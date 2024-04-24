9cc216de85fb7ab7ae87f5cc7faac92d6eb16093
Willian Binda <willianbinda122@gmail.com>
2024-04-24 10:24:15 -0300

    feat: modificação routes.rb

--- app/controllers/node_red_controller.rb
+++ app/controllers/node_red_controller.rb
@@ -1,6 +1,11 @@
 class NodeRedController < ApplicationController
   before_action :authenticate_user!
   # load_and_authorize_resource
+  def authenticate
+    puts "\n\nExecutou a função autenticate\n\n"
+    head :ok
+  end
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :acessos
-  get 'node_red/index'
   devise_for :users
   # devise_for :users, controllers: { sessions: 'users/sessions' }
   devise_scope :user do
@@ -10,18 +8,21 @@ Rails.application.routes.draw do
   resources :addresses
   resources :contacts
   resources :kinds
+  resources :acessos
+  resources :homes
   # resources :kinds, only: [:edit]
   # resources :kinds, except: [:edit,:destroy]
-  resources :homes
-  get 'authenticate', to: 'auth#authenticate'
+  get 'node_red/index'
+  get 'node_red/authenticate'
+  # get 'authenticate', to: 'auth#authenticate'
 
   # get 'pagina2', to: 'kinds#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-
-
+  
+  
   # Defines the root path route ("/")
   root "homes#index"
 end
